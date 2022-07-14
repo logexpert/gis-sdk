e3f94b4c96b6370ac3828039c9069bb4dc37d585
Enhed <s.zorin@logexpert.ru>
2022-07-14 17:20:01 +0300

    Add LatLon type and export ObjectStateInfo

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,8 +1,14 @@
 export * from './gis_object'
 export * from './track_point'
+export * from './object_state_info'
 
 export type Item = Record<string, any>
 export type ID = number | string
+
+export type LatLon = {
+    Latitude: number
+    Longitude: number
+}
 
 export type PartialItem<T extends Item> = {
     [Key in keyof T]?: T[Key] extends Item ? PartialItem<T[Key]> : T[Key]
